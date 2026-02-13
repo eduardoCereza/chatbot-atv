96660dfe8cb2a64eab43db86fa9816c85ec0fbf3
Eduardo <eduardo.cereza@outlook.com>
2026-02-12 21:48:18 -0300

    finalizando chat bot

--- app.py
+++ app.py
@@ -5,14 +5,24 @@ app = Flask(__name__)
 
 socketio = SocketIO(app)
 
+mensagem = ''
+
 @app.route("/chat", methods=['GET'])
 def render_page():
     return render_template('index.html',
                            host='http://127.0.0.1:5000')
 
-@socketio.on('connect')
-def handle_connect():
-    print("Client connected to the server!")
+@socketio.on('message')
+def post_message(msg):
+    
+    if not msg:
+        return
+    
+    
+    socketio.send(msg)
+    
+    
+    
     
 
 @socketio.on('disconnect')
